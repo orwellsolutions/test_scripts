62cea1514591d79fe9f0729a800b9fdc1fbc075e
Paul Gosden <paul@easyestiamte.co.uk>
2023-02-22 11:00:52 +0000

    new

--- migrate.py
+++ migrate.py
@@ -9,17 +9,16 @@ auth = ("your-nexus-username", "your-nexus-password")
 # Set up the variables for the local directory where artifacts will be downloaded
 local_dir = "your-local-directory-path"
 
-# Create the local directory if it doesn't already exist
-if not os.path.exists(local_dir):
-    os.makedirs(local_dir)
-
 # Set up the variables for the Nexus 3 API
 url = f"{nexus_url}/service/rest/v1/search/assets"
 params = {"repository": repository}
 
+# Ignore SSL certificate validation
+verify_ssl = False
+
 # Iterate through all the artifacts in the repository and download them
 while url:
-    response = requests.get(url, auth=auth, params=params)
+    response = requests.get(url, auth=auth, params=params, verify=verify_ssl)
     data = response.json()
     items = data["items"]
     
@@ -27,12 +26,18 @@ while url:
         download_url = item["downloadUrl"]
         artifact_path = item["path"].lstrip("/")
         artifact_filename = os.path.basename(artifact_path)
-        local_path = os.path.join(local_dir, artifact_filename)
+        local_path = os.path.join(local_dir, artifact_path)
+        
+        # Create the directory hierarchy in the local directory
+        local_dirname = os.path.dirname(local_path)
+        if not os.path.exists(local_dirname):
+            os.makedirs(local_dirname)
         
         print(f"Downloading {artifact_path} to {local_path}")
-        response = requests.get(download_url, auth=auth)
+        response = requests.get(download_url, auth=auth, verify=verify_ssl)
         
         with open(local_path, "wb") as f:
             f.write(response.content)
     
     url = data["continuationToken"]
+

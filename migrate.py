6f585815b500f4d4e576b935e03b07702f4a5f1b
Paul Gosden <paul@easyestiamte.co.uk>
2023-02-22 10:46:29 +0000

    test

---
+++ migrate.py
@@ -0,0 +1,38 @@
+import os
+import requests
+
+# Set up the variables for the Nexus 3 repository
+nexus_url = "https://your-nexus-repository-url.com"
+repository = "your-repository-name"
+auth = ("your-nexus-username", "your-nexus-password")
+
+# Set up the variables for the local directory where artifacts will be downloaded
+local_dir = "your-local-directory-path"
+
+# Create the local directory if it doesn't already exist
+if not os.path.exists(local_dir):
+    os.makedirs(local_dir)
+
+# Set up the variables for the Nexus 3 API
+url = f"{nexus_url}/service/rest/v1/search/assets"
+params = {"repository": repository}
+
+# Iterate through all the artifacts in the repository and download them
+while url:
+    response = requests.get(url, auth=auth, params=params)
+    data = response.json()
+    items = data["items"]
+    
+    for item in items:
+        download_url = item["downloadUrl"]
+        artifact_path = item["path"].lstrip("/")
+        artifact_filename = os.path.basename(artifact_path)
+        local_path = os.path.join(local_dir, artifact_filename)
+        
+        print(f"Downloading {artifact_path} to {local_path}")
+        response = requests.get(download_url, auth=auth)
+        
+        with open(local_path, "wb") as f:
+            f.write(response.content)
+    
+    url = data["continuationToken"]

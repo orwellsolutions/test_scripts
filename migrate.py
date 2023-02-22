9ad14cdfb4249085446978df220127b427efd3d7
Paul Gosden <paul@easyestiamte.co.uk>
2023-02-22 11:08:20 +0000

    test

--- migrate.py
+++ migrate.py
@@ -13,12 +13,9 @@ local_dir = "your-local-directory-path"
 url = f"{nexus_url}/service/rest/v1/search/assets"
 params = {"repository": repository}
 
-# Ignore SSL certificate validation
-verify_ssl = False
-
 # Iterate through all the artifacts in the repository and download them
 while url:
-    response = requests.get(url, auth=auth, params=params, verify=verify_ssl)
+    response = requests.get(url, auth=auth, params=params)
     data = response.json()
     items = data["items"]
     
@@ -26,15 +23,16 @@ while url:
         download_url = item["downloadUrl"]
         artifact_path = item["path"].lstrip("/")
         artifact_filename = os.path.basename(artifact_path)
+        artifact_dirname = os.path.dirname(artifact_path)
         local_path = os.path.join(local_dir, artifact_path)
         
         # Create the directory hierarchy in the local directory
-        local_dirname = os.path.dirname(local_path)
+        local_dirname = os.path.join(local_dir, artifact_dirname)
         if not os.path.exists(local_dirname):
             os.makedirs(local_dirname)
         
         print(f"Downloading {artifact_path} to {local_path}")
-        response = requests.get(download_url, auth=auth, verify=verify_ssl)
+        response = requests.get(download_url, auth=auth)
         
         with open(local_path, "wb") as f:
             f.write(response.content)

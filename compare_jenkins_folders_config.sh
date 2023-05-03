7073767271cadebd1b0e7738a4eb21e56798b6bf
Paul Gosden <paul.gosden@orwellsolutions.com>
2023-05-03 11:29:44 +0100

    Update compare_jenkins_folders_config.sh

--- compare_jenkins_folders_config.sh
+++ compare_jenkins_folders_config.sh
@@ -2,7 +2,7 @@
 
 # Ensure that two arguments are provided
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <jenkins_folder1> <jenkins_folder2>"
+    echo "Usage: $0 <folder1> <folder2>"
     exit 1
 fi
 
@@ -13,32 +13,34 @@ if [ ! -d "$1" ] || [ ! -d "$2" ]; then
 fi
 
 # Assign the folder paths to variables
-jenkins_folder1="$1"
-jenkins_folder2="$2"
+folder1="$1"
+folder2="$2"
 
-# Find all subfolders containing config.xml in both directories
-find "$jenkins_folder1" "$jenkins_folder2" -type f -name 'config.xml' | sed "s#\(.*\)/config.xml#\1#g" | sort > folders_with_config.txt
+# Find all subfolders in both directories
+find "$folder1" "$folder2" -type d | sort | uniq > all_folders.txt
 
 # Loop through each folder and compare config.xml files
 while read -r folder_path; do
-    folder_name=$(basename "$folder_path")
-    config_file1="$jenkins_folder1/$folder_name/config.xml"
-    config_file2="$jenkins_folder2/$folder_name/config.xml"
+    # Remove the base folder path to get the relative path
+    relative_path=${folder_path#*$folder1}
+    relative_path=${relative_path#*$folder2}
+    config_file1="$folder1$relative_path/config.xml"
+    config_file2="$folder2$relative_path/config.xml"
 
     if [ -f "$config_file1" ] && [ -f "$config_file2" ]; then
         # Both config.xml files exist, compare them
         diff_output=$(diff -q "$config_file1" "$config_file2")
         if [ -n "$diff_output" ]; then
-            echo "Difference found in $folder_name:"
+            echo "Difference found in $relative_path:"
             echo "$diff_output"
         fi
     else
-        # One of the config.xml files is missing
-        echo "config.xml missing in $folder_name:"
+        # One of the config.xml files is missing or both are missing
+        echo "config.xml missing or not present in $relative_path:"
         [ ! -f "$config_file1" ] && echo "  $config_file1"
         [ ! -f "$config_file2" ] && echo "  $config_file2"
     fi
-done < folders_with_config.txt
+done < all_folders.txt
 
 # Clean up temporary file
-rm folders_with_config.txt
+rm all_folders.txt

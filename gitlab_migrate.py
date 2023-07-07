fb239bb7699cdb5e68e74236a529393dbdbadf57
Paul Gosden <paul.gosden@orwellsolutions.com>
2023-07-07 08:00:36 +0100

    Update gitlab_migrate.py

--- gitlab_migrate.py
+++ gitlab_migrate.py
@@ -1,104 +1,101 @@
 import requests
 import time
-import os
 import subprocess
-import urllib3
+import base64
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+source_gitlab_instance = input("Please enter the source GitLab instance URL (default: PUT GITLAB SOURCE URL HERE): ") or "PUT GITLAB SOURCE URL HERE"
+destination_gitlab_instance = input("Please enter the destination GitLab instance URL: ")
+source_project_path = input("Please enter the source project path: ")
+destination_project_path = input("Please enter the destination project path: ")
 
-# Set default value
-source_gitlab_instance = "PUT GIT HUB URL HERE"
+source_private_token = input("Please enter the source private token: ")
+destination_private_token = input("Please enter the destination private token: ")
 
-# Use default value in input prompt
-source_gitlab_instance = input(f"Enter the URL of the original GitLab instance (default is '{source_gitlab_instance}'): ") or source_gitlab_instance
+bash_command = f"your_bash_command {source_project_path}"
 
-source_token = input("Enter the access token for the original GitLab instance: ")
-target_token = input("Enter the access token for the new GitLab instance: ")
-
-source_username = input("Enter the username for the original GitLab instance: ")
-source_project_name = input("Enter the project name for the original GitLab instance: ")
-
-target_username = input("Enter the username for the new GitLab instance: ")
-target_project_name = input("Enter the project name for the new GitLab instance: ")
-
-# Check if the .gitlab-ci.yml file exists in the root of the repository
-headers = {"PRIVATE-TOKEN": source_token}
-file_check_url = f"{source_gitlab_instance}/api/v4/projects/{source_username}%2F{source_project_name}/repository/files/.gitlab-ci.yml"
-
-response = requests.get(file_check_url, headers=headers, verify=False)
-
-if response.status_code == 200:
-    print("The .gitlab-ci.yml file exists in the root of the repository. Aborting operation.")
-    exit()
-
-# Run a bash command against the original repo
-bash_command = "bash_command {}".format(source_project_name)  # Replace "bash_command" with your command
-process = subprocess.Popen(bash_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-# Print output in real-time and store it in a list
-output = []
-for line in iter(process.stdout.readline, b''):
-    line = line.decode().strip()
-    print(line)
-    output.append(line)
-
-process.stdout.close()
-process.wait()
+# Run bash command and get output
+output = subprocess.check_output(['bash','-c', bash_command]).decode().split('\n')
 
 # Check the output
-if "No secrets found" not in output:
+if any("No secrets found" in line for line in output):
+    print("'No secrets found' found in the repository. Continuing operation.")
+else:
     print("Secrets found in the repository. Aborting operation.")
     exit()
 
-# Step 1: Export the project from the source GitLab instance
-export_url = f"{source_gitlab_instance}/api/v4/projects/{source_username}%2F{source_project_name}/export"
-response = requests.post(export_url, headers=headers, verify=False)
+# Check if .gitlab-ci.yml exists in the repository
+response = requests.get(f"{source_gitlab_instance}/{source_project_path}/-/blob/master/.gitlab-ci.yml", headers={"PRIVATE-TOKEN": source_private_token}, verify=False)
+
+if response.status_code == 200:
+    print("The repository contains a .gitlab-ci.yml file. Aborting operation.")
+    exit()
+
+headers = {"PRIVATE-TOKEN": source_private_token}
+
+# Request the export
+response = requests.post(f"{source_gitlab_instance}/api/v4/projects/{source_project_path}/export", headers=headers, verify=False)
 
 if response.status_code == 202:
     print("Export started successfully.")
 else:
-    print("Failed to start export.")
+    print(f"Failed to start export: {response.json()}")  # print the API response
+    exit()
 
-# Step 2: Download the export file
+# Get the export download link
 while True:
-    response = requests.get(export_url, headers=headers, verify=False)
+    response = requests.get(f"{source_gitlab_instance}/api/v4/projects/{source_project_path}/export", headers=headers, verify=False)
+    print(response.json())  # print the API response
     if response.json()["export_status"] == "finished":
         download_url = response.json()["download_url"]
         break
     time.sleep(1)
 
-response = requests.get(f"{source_gitlab_instance}{download_url}", headers=headers, stream=True, verify=False)
+# Download the exported project
+response = requests.get(download_url, headers=headers, verify=False)
 
 with open("project_export.tar.gz", "wb") as f:
-    for chunk in response.iter_content(chunk_size=8192):
-        f.write(chunk)
+    f.write(response.content)
 
-# Step 3: Create a new project on the target GitLab instance
-headers = {"PRIVATE-TOKEN": target_token}
-create_project_url = f"{target_gitlab_instance}/api/v4/projects"
+headers = {"PRIVATE-TOKEN": destination_private_token}
 
-data = {"name": target_project_name, "namespace_id": target_username}
-response = requests.post(create_project_url, headers=headers, json=data, verify=False)
-
-if response.status_code == 201:
-    print("Project created successfully.")
-else:
-    print("Failed to create project.")
-
-# Step 4: Import the project to the target GitLab instance
-target_project_id = response.json()["id"]
-
-headers = {"PRIVATE-TOKEN": target_token}
-import_url = f"{target_gitlab_instance}/api/v4/projects/{target_project_id}/import"
-
+# Import the project into the new GitLab instance
 with open("project_export.tar.gz", "rb") as f:
-    data = {"namespace": target_username, "file": f}
-    response = requests.post(import_url, headers=headers, files=data, verify=False)
+    data = {
+        "file": f,
+        "path": destination_project_path,
+        "name": destination_project_path,
+        "namespace": destination_project_path.split('/')[0]
+    }
+    response = requests.post(f"{destination_gitlab_instance}/api/v4/projects/import", headers=headers, data=data, verify=False)
 
 if response.status_code == 201:
-    print("Import started successfully.")
+    print("Project imported successfully.")
 else:
-    print("Failed to start import.")
+    print(f"Failed to import project: {response.json()}")  # print the API response
+    exit()
 
-# Remove the downloaded file
-os.remove("project_export.tar.gz")
+# Wait for the import to finish
+time.sleep(10)  # Adjust this sleep time as necessary
+
+# Update README.md file in the new repository
+new_content = f"This project has been moved to {destination_gitlab_instance}/{destination_project_path}"
+b64_content = base64.b64encode(new_content.encode()).decode()  # README content must be base64 encoded
+
+data = {
+    "branch": "master",
+    "commit_message": "Updated README with new project location",
+    "actions": [
+        {
+            "action": "update",
+            "file_path": "README.md",
+            "content": b64_content
+        }
+    ]
+}
+
+response = requests.post(f"{destination_gitlab_instance}/api/v4/projects/{destination_project_path}/repository/commits", headers=headers, json=data, verify=False)
+
+if response.status_code == 201:
+    print("README.md updated successfully.")
+else:
+    print(f"Failed to update README.md: {response.json()}")  # print the API response
